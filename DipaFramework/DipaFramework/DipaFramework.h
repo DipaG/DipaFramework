fd864ea3362ea4400dc135409ce73ec722b62ecf
Dipa Tapsale <ghorpade.dipa@gmail.com>
2019-03-05 14:44:55 +0530

    Initial commit

---
+++ DipaFramework/DipaFramework/DipaFramework.h
@@ -0,0 +1,19 @@
+//
+//  DipaFramework.h
+//  DipaFramework
+//
+//  Created by Dipa Tapsale on 05/03/19.
+//  Copyright © 2019 Dipa Tapsale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DipaFramework.
+FOUNDATION_EXPORT double DipaFrameworkVersionNumber;
+
+//! Project version string for DipaFramework.
+FOUNDATION_EXPORT const unsigned char DipaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DipaFramework/PublicHeader.h>
+
+
---
+++ DipaFramework/DipaFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  DipaFramework
+//
+//  Created by Dipa Tapsale on 05/03/19.
+//  Copyright © 2019 Dipa Tapsale. All rights reserved.
+//
+
+import Foundation
+class Service {
+    
+    private init(){
+        
+    }
+    public static func doSomething() -> String{
+        return "Hello World!"
+    }
+}
